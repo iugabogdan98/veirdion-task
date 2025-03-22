0ebca7c965e364e87c0358a04295b3f54b2af9a1
Bogdan Iuga <iugabogdan98@gmail.com>
2025-03-22 13:11:53 +0200

    add puppeteer to try to work around cloudfare, cleaner code

--- src/services/scrape.ts
+++ src/services/scrape.ts
@@ -1,17 +1,30 @@
 import {Request, Response} from "express";
-import axios, {AxiosError} from "axios";
+import axios, {AxiosError, AxiosResponse} from "axios";
 import * as cheerio from "cheerio";
 import {parse} from "csv-parse";
 import * as fs from "fs";
 import {configs} from "../configs";
 import pLimit from "p-limit";
+import puppeteer from 'puppeteer';
 
 
 interface ScrapeResult {
+    success: boolean;
     url: string;
     title?: string;
     error?: string;
+    errorCode: string;
 }
+
+const outputFailsByCodeInFile = (fails: Map<string, ScrapeResult[]>) => {
+    let failsString = '';
+    fails.forEach((value, key) =>
+        failsString += `${key}: ${value.length} error\n ${value.map(elem => `${elem.url} -> ${elem.error}\n`)}\n`);
+
+
+    fs.writeFileSync("failed_requests_log.txt", failsString);
+}
+
 
 const parseDomainsFromCsv = async (path: string): Promise<string[]> => {
     const domains: string[] = [];
@@ -28,60 +41,67 @@ const parseDomainsFromCsv = async (path: string): Promise<string[]> => {
     return domains;
 };
 
-const outputFailsByCodeInFile = (fails: Map<string, ScrapeResult[]>) => {
-    let failsString = '';
-    fails.forEach((value, key) =>
-        failsString += `${key}: ${value.length} error\n ${value.map(elem => `${elem.url} -> ${elem.error}\n`)}\n`);
+function handleAndParseResponse(response: AxiosResponse, url: string) {
+    const $ = cheerio.load(response.data);
+    const title = $('title').text().trim();
+    console.log(`Scraped ${url}: ${title}`);
 
+    return {success: true, url, title, errorCode: ''};
+}
 
-    fs.writeFileSync("failed_requests_log.txt", failsString);
+function handleErrorAxios(error: AxiosError, url: string) {
+    const errorMsg = error.message ?? "Unknown error";
+    const errorCode: string = error?.code ?? "Unknown error";
+    console.error(`Error scraping ${url}: ${errorMsg}`);
+
+    return ({success: false, url, error: errorMsg, errorCode});
 }
 
 export const scrape = async (req: Request, res: Response) => {
     const results: ScrapeResult[] = [];
     const fails: Map<string, ScrapeResult[]> = new Map();
+    let failsCount = 0;
+
+    const axiosOptions = {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+        },
+        timeout: configs.WEBSITE_READ_TIMEOUT,
+    }
 
     const limit = pLimit(configs.P_LIMIT);
-
-
     const urls: string[] = (await parseDomainsFromCsv(configs.COMPANIES_PATH));
 
     const promises = urls.map(url =>
         limit(() =>
-            axios.get(url, {
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                    'Accept-Language': 'en-US,en;q=0.5',
-                    'Connection': 'keep-alive',
-                    'Upgrade-Insecure-Requests': '1',
-                },
-                timeout: configs.WEBSITE_READ_TIMEOUT,
-            })
+            axios.get(url, axiosOptions)
                 .then(response => {
-                    const $ = cheerio.load(response.data);
-                    const title = $('title').text().trim();
-
-                    results.push({url, title});
-                    console.log(`Scraped ${url}: ${title}`);
-
-                    return {url, title};
+                    return handleAndParseResponse(response, url);
                 })
                 .catch(error => {
-                    const errorMsg = error instanceof AxiosError ? error.message : "Unknown error";
-                    const errorCode: string = error instanceof AxiosError ? (error?.code ?? "Unknown error") : "Unknown error";
-                    const failValue = fails.get(errorCode) || [];
-                    failValue.push({url, error: errorMsg})
-                    fails.set(errorCode, failValue);
-                    console.error(`Error scraping ${url}: ${errorMsg}`);
+                    return handleErrorAxios(error, url);
                 })
         )
     );
 
-    await Promise.all(promises);
+    const outcomes = await Promise.all(promises);
 
-    let failsCount = 0;
-    fails.forEach(fail => failsCount += fail.length);
+    outcomes.forEach(outcome => {
+        if (outcome.success) {
+            results.push(outcome);
+        } else {
+            if (!fails.has(outcome.errorCode)) {
+                fails.set(outcome.errorCode, [outcome]);
+            } else {
+                fails.get(outcome.errorCode)?.push(outcome);
+            }
+            failsCount++;
+        }
+    })
 
     res.status(200).json({
         successCount: results.length,

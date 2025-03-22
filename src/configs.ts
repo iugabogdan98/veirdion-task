048f8312f19c997b58a1eb33b1b97c39b46e5a07
Bogdan Iuga <iugabogdan98@gmail.com>
2025-03-22 10:21:48 +0200

    group errors, log in file

--- src/configs.ts
+++ src/configs.ts
@@ -1,7 +1,9 @@
+const os = require('os');
+
 export const configs = {
     HTTP_NON_SECURE: "http://",
     HTTP_SECURE: "https://",
-    P_LIMIT: 20,
+    P_LIMIT: os.cpus().length * 3,
     WEBSITE_READ_TIMEOUT: 20000,
     COMPANIES_PATH: './src/input/sample-websites.csv',
     COMPANIES_WITH_NAMES_PATH: './input/sample-websites-company-names.csv',
--- src/services/scrape.ts
+++ src/services/scrape.ts
@@ -1,10 +1,11 @@
 import {Request, Response} from "express";
-import axios from "axios";
+import axios, {AxiosError} from "axios";
 import * as cheerio from "cheerio";
 import {parse} from "csv-parse";
 import * as fs from "fs";
 import {configs} from "../configs";
 import pLimit from "p-limit";
+
 
 interface ScrapeResult {
     url: string;
@@ -27,19 +28,33 @@ const parseDomainsFromCsv = async (path: string): Promise<string[]> => {
     return domains;
 };
 
+const outputFailsByCodeInFile = (fails: Map<string, ScrapeResult[]>) => {
+    let failsString = '';
+    fails.forEach((value, key) =>
+        failsString += `${key}: ${value.length} error\n ${value.map(elem => `${elem.url} -> ${elem.error}\n`)}\n`);
+
+
+    fs.writeFileSync("failed_requests_log.txt", failsString);
+}
+
 export const scrape = async (req: Request, res: Response) => {
     const results: ScrapeResult[] = [];
-    const fails: ScrapeResult[] = [];
+    const fails: Map<string, ScrapeResult[]> = new Map();
+
     const limit = pLimit(configs.P_LIMIT);
 
 
-    const urls: string[] = await parseDomainsFromCsv(configs.COMPANIES_PATH);
+    const urls: string[] = (await parseDomainsFromCsv(configs.COMPANIES_PATH));
 
     const promises = urls.map(url =>
         limit(() =>
             axios.get(url, {
                 headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                    'Accept-Language': 'en-US,en;q=0.5',
+                    'Connection': 'keep-alive',
+                    'Upgrade-Insecure-Requests': '1',
                 },
                 timeout: configs.WEBSITE_READ_TIMEOUT,
             })
@@ -53,9 +68,11 @@ export const scrape = async (req: Request, res: Response) => {
                     return {url, title};
                 })
                 .catch(error => {
-                    const errorMsg = error instanceof Error ? error.message : "Unknown error";
-
-                    fails.push({url, error: errorMsg});
+                    const errorMsg = error instanceof AxiosError ? error.message : "Unknown error";
+                    const errorCode: string = error instanceof AxiosError ? (error?.code ?? "Unknown error") : "Unknown error";
+                    const failValue = fails.get(errorCode) || [];
+                    failValue.push({url, error: errorMsg})
+                    fails.set(errorCode, failValue);
                     console.error(`Error scraping ${url}: ${errorMsg}`);
                 })
         )
@@ -63,13 +80,16 @@ export const scrape = async (req: Request, res: Response) => {
 
     await Promise.all(promises);
 
+    let failsCount = 0;
+    fails.forEach(fail => failsCount += fail.length);
+
     res.status(200).json({
         successCount: results.length,
-        failCount: fails.length,
+        failCount: failsCount,
         successResults: results
     });
 
     console.log('Scraping completed');
     console.log('Results #:', results.length);
-    console.log('Fails #:', fails.length);
+    outputFailsByCodeInFile(fails);
 }

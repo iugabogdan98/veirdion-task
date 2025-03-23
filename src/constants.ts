247da54e7adfa084f08fd0f73e7dfeb831339ae8
Bogdan Iuga <iugabogdan98@gmail.com>
2025-03-23 19:06:50 +0200

    twaek regex, include more variations of phone number, reached 400 hits :D

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const constants = {
     phoneNumberRegex: {
-        default: /\b(?:\+?\d{1,2}\s?)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}\b/g,
+        default: /\b(?:\+?\d{1,2}\s?)?\(?\d{2,4}\)?[-.\s]?\d{2,4}[-.\s]?\d{2,4}\b/g,
     },
     socialMediaRegex: {},
     contactPageRegex: {
